491f0e012053cedcfa898e9051dd7da2132db48f
Anonta <SH-anonta@users.noreply.github.com>
2018-09-30 19:59:10 +0600

    Reorganize project files Add new entry in .gitignore

--- src/main.py
+++ src/main.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     # url = 'https://www.flickr.com/photos/mrkotek'
     url = 'https://www.flickr.com/photos/theogeo/'
 
-    folder_name = 'theogeo4'
+    folder_name = 'theogeo10'
     export_path = r'G:\Flickr'
     export_path = os.path.join(export_path, folder_name)
     createDirectory(export_path)
@@ -52,8 +52,8 @@ if __name__ == '__main__':
     initializeFlickrAPI()
     configureLogging(export_path)
 
-    start_page = 61
-    end_page   = 90
+    start_page = 301
+    end_page   = 345
 
     logging.info('-------------------Start-------------------')
     logging.info('Attempting to download photos from page {} to {}'.format(start_page, end_page))
